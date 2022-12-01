968f45008af60d9731991d945657fb508e565cb1
Dexter <dex.chavez@yahoo.com>
2022-12-01 14:18:58 +0800

    new update

--- js/main.js
+++ js/main.js
@@ -117,3 +117,13 @@
     
 })(jQuery);
 
+// init Isotope
+var $grid = $('#product-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
